c43e543fab8de947462f7f5294b5e1e81043537a
tse1218 <tse1218@gmail.com>
2014-08-26 12:10:40 +0800

    php test
    try print

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>print test</title>
+		<meta charset="utf-8">
+		<link href="default.css" rel="stylesheet" type="text/css" >
+	</head>
+	<body>
+		<div class="NoPrint">
+			<p align="center">123456</p>
+			<p align="center"><a href="javascript:void(window.print())">列印</a></p>
+		</div>
+		<div class="JustPrint">
+			abcde
+		</div>
+	</body>
+</html>
